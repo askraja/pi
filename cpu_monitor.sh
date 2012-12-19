b33fca2bb46a78186ad52d1e512feb6b5879ca8a
Karthik Gaekwad <karthik_gaekwad@mentor.com>
2012-12-19 11:43:48 -0800

    Create cpu_monitor.sh
    Created to monitor rpi temperature

---
+++ cpu_monitor.sh
@@ -0,0 +1,10 @@
+IN=/opt/vc/bin/vcgencmd measure_temp
+#IN="temp=53.0'C"
+echo "$IN"
+
+##Let's convert
+IN2=${IN#temp=}
+IN3=${IN2%\'C}
+TEMP_IN_F=`echo "$IN3 * 1.8 + 32" | bc`
+##Print it out
+echo "Processor Temperature:" $TEMP_IN_F"'F," $IN3"'C"
