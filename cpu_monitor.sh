a03de37bfbd82ae8951d3109c7f361724aee8549
Karthik Gaekwad <karthik_gaekwad@mentor.com>
2012-12-19 14:09:27 -0600

    Update cpu_monitor.sh

--- cpu_monitor.sh
+++ cpu_monitor.sh
@@ -3,8 +3,8 @@ IN=`/opt/vc/bin/vcgencmd measure_temp`
 echo "$IN"
 
 ##Let's convert
-IN2=${IN#temp=}
-IN3=${IN2%\'C}
-TEMP_IN_F=`echo "$IN3 * 1.8 + 32" | bc`
+#IN2=${IN#temp=}
+#IN3=${IN2%\'C}
+#TEMP_IN_F=`echo "$IN3 * 1.8 + 32" | bc`
 ##Print it out
-echo "Processor Temperature:" $TEMP_IN_F"'F," $IN3"'C"
+#echo "Processor Temperature:" $TEMP_IN_F"'F," $IN3"'C"
